6216fb372c0608e0bda5a4e5de6a26d2b153825c
Igor Bereza <igor.bereza@gmail.com>
2021-11-14 12:35:57 +0200

    added base api for users

--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import CustomUser
+from .serializers import UsersModelSerializer
+
+
+class UsersModelViewSet(ModelViewSet):
+    queryset = CustomUser.objects.all()
+    serializer_class = UsersModelSerializer
