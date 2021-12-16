6f5da2bca4efca2efdd93051259ca1d5a39596de
Igor Bereza <igor.bereza@gmail.com>
2021-12-16 09:55:05 +0200

    added documentation

--- todo_project/urls.py
+++ todo_project/urls.py
@@ -15,11 +15,26 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
 
 from project.views import ProjectModelViewSet, TodoModelViewSet
 from users.views import UsersModelViewSet
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Library",
+        default_version='v2',
+        description="Documentation to out project",
+        contact=openapi.Contact(email="admin@admin.local"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 router = DefaultRouter()
 router.register('users', UsersModelViewSet)
@@ -30,5 +45,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include(router.urls)),
-    path('api-token-auth/', views.obtain_auth_token)
+    path('api-token-auth/', views.obtain_auth_token),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('swagger/<str:format>/', schema_view.with_ui()),
 ]
